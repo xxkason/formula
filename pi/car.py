d03a95c29c92a4614d8b6b008e9afc0b46e060d4
Kason <xxkason@gmail.com>
2018-01-14 11:11:20 +0800

    add RaceCar class

--- pi/car.py
+++ pi/car.py
@@ -18,7 +18,8 @@ FAKE_PWM_4 = 38 # GPIO20
 class ToyCar(object):
     TURNING_TIME = 0.5
 
-    def __init__(self, wheel_motor_number, wheel_pwm_pin, driver_motor_num, driver_pwm_pin, default_wheel_speed = 60):
+    def __init__(self, wheel_motor_number, wheel_pwm_pin, 
+    driver_motor_num, driver_pwm_pin, default_wheel_speed = 60):
         self.__wheel = Motor(wheel_motor_number, wheel_pwm_pin)
         self.__drive_motor = Motor(driver_motor_num, driver_pwm_pin)
         self.__wheel.setSpeed(default_wheel_speed)
@@ -55,12 +56,59 @@ class ToyCar(object):
 
 # RaceCar class
 # 1个舵机，5V输出，PWM控制转向角度
-# 4个驱动电机
+# 4个驱动电机，驱动不能调速，全速转动
 class RaceCar(object):
-    def __init__(self):
+    def __init__(self，wheel_pwm_pin, 
+    lfm_num, lfm_pwm_pin, 
+    lbm_num, lbm_pwm_pin,
+    rfm_num, rfm_pwm_pin,
+    rbm_num, rbm_pwm_pin,
+    lfm_fake_pwm = False, lbm_fake_pwm = False,
+    rfm_fake_pwm = False, rbm_fake_pwm = False
+    ):
+        self.__leftFront = Motor(lfm_num, lfm_pwm_pin, lfm_fake_pwm)
+        self.__leftBehind = Motor(lbm_num, lbm_pwm_pin, lbm_fake_pwm)
+        self.__rightFront = Motor(rfm_num, rfm_pwm_pin, rfm_fake_pwm)
+        self.__rightBehind = Motor(rbm_num, rbm_pwm_pin, rbm_fake_pwm)
+        GPIO.setup(wheel_pwm_pin, GPIO.OUT)
+        self.__wheelPWM = GPIO.PWM(wheel_pwm_pin, 1000)
 
+    def goForward(self):
+        self.__leftFront.stop()
+        self.__rightFront.stop()
+        self.__leftBehind.stop()
+        self.__rightBehind.stop()
+        self.__leftFront.run(Motor.FORWARD)
+        self.__rightFront.run(Motor.FORWARD)
+        self.__rightBehind.run(Motor.FORWARD)
+        self.__leftBehind.run(Motor.FORWARD)
+
+    def goBackward(self):
+        self.__leftFront.stop()
+        self.__rightFront.stop()
+        self.__leftBehind.stop()
+        self.__rightBehind.stop()
+        self.__leftFront.run(Motor.BACKWARD)
+        self.__rightFront.run(Motor.BACKWARD)
+        self.__rightBehind.run(Motor.BACKWARD)
+        self.__leftBehind.run(Motor.BACKWARD)
+
+    def stop(self):
+        self.__leftFront.stop()
+        self.__rightFront.stop()
+        self.__leftBehind.stop()
+        self.__rightBehind.stop()
+        self.__wheelPWM.stop()
+
+    def gear(self, speed):
+        pass
+
+    def wheelControl(self, angle):
+        self.__wheelPWM.start(angle)
 
 toyCar = ToyCar(1, WHEEL_PWM, 2, PWM_2)
+raceCar = RaceCar(WHEEL_PWM, 1, FAKE_PWM_1, 2, FAKE_PWM_2, 
+                  3, FAKE_PWM_3, 4, FAKE_PWM_4, True, True, True, True)
 
 # try:
 #     while(True):
