17f2478ab4cb993f9c295130fc2b8ee372111907
Kai Peng <xxkason@gmail.com>
2018-02-11 23:04:51 +0800

    add learning mode and auto replay mode

--- arduino/formula/formula.ino
+++ arduino/formula/formula.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 //#include <SoftwareSerial.h>
 //#include "Car_2DC.h"
 // Gamepad msg info
@@ -8,11 +9,13 @@
 
       E               M
       F               N
+A,B,C,D,E,F,M,N continuous send cmd when pressed
 */
 #include "Car_4WD.h"
 
 #define GAMEPAD_BAUD_RATE 9600
 #define BLUETOOTH_BAUD_RATE 9600
+#define INDICATOR_PIN 2
 
 enum PadMode
 {
@@ -24,9 +27,9 @@ enum PadMode
 
 enum CarMode
 {
-  AUTO = 0,
-  MANUAL = 1,
-  LEARN = 2
+  MANUAL = 0,
+  LEARN = 1,
+  AUTO = 2
 };
 
 //SoftwareSerial softSerial(10, 11); // RX, TX
@@ -35,10 +38,10 @@ Car *btcar;
 // Car_2DC_L298N car298n(2, 4, 6, 7, 8, 9);
 // Car_2DC_L2HBd car2hbd(3, 5, 6, 9);
 Car_4WD car4wd(1, 2, 3, 4, 10);
-bool autoMode = false;
 PadMode manual_mode = LEFT_ANALOG_STICK;
 CarMode control_mode = MANUAL;
-
+int rom_addr = 0;
+unsigned long begin_time;
 
 void setup()
 {
@@ -50,72 +53,154 @@ void setup()
   // Serial.println("Hello, monitor");
   car4wd.attachWheel();
   btcar = &car4wd;
+  pinMode(INDICATOR_PIN, OUTPUT);
+  digitalWrite(INDICATOR_PIN, LOW);
   Serial.setTimeout(300);
 }
 
 void loop()
 {
-  manual_control();
+  control_car();
 }
 
-void manual_control()
+void control_car()
 {
-  char cmd[1];
-  if (Serial.readBytes(cmd, 1))
+  char msg[1];
+  if (Serial.readBytes(msg, 1))
   {
-    if (cmd[0] == 'G')
+    char cmd = msg[0];
+    if (cmd == 'G')
     {
-      manual_mode = PadMode((manual_mode + 1) % 4);
+      control_mode = CarMode((control_mode + 1) % 3);
+      switch (control_mode)
+      {
+        case MANUAL:
+          digitalWrite(INDICATOR_PIN, LOW);
+          break;
+        case LEARN:
+          clear_eeprom();
+          rom_addr = 0;
+          begin_time = 0;
+          digitalWrite(INDICATOR_PIN, HIGH);
+          break;
+        case AUTO:
+          digitalWrite(INDICATOR_PIN, HIGH);
+          break;
+      }
     }
     else
     {
-      char msg = cmd[0];
-      switch (manual_mode)
+      switch (control_mode)
       {
-        case LEFT_ANALOG_STICK:
-          leftstick_control(msg);
+        case MANUAL:
+          manual_control(cmd);
           break;
-        case LEFT_RIGHT_STICK:
-          leftrightstick_control(msg);
+        case AUTO:
+          if (cmd == 'H')
+          {
+            auto_control();
+          }
           break;
-        case LEFT_PAD_KEY:
-          leftkey_control(msg);
-          break;
-        case RIGHT_PAD_KEY:
-          rightkey_control(msg);
+        case LEARN:
+          learning(cmd);
           break;
       }
     }
   }
-  else if (manual_mode == LEFT_PAD_KEY)
+  else if (control_mode == MANUAL && manual_mode == LEFT_PAD_KEY)
   {
     btcar->stop();
+  }
+  else if (control_mode == LEARN)
+  {
+    digitalWrite(INDICATOR_PIN, LOW);
+    delay(500);
+    digitalWrite(INDICATOR_PIN, HIGH);
+  }
+}
+
+void manual_control(char cmd)
+{
+  if (cmd == 'H')
+  {
+    manual_mode = PadMode((manual_mode + 1) % 4);
+  }
+  else
+  {
+    switch (manual_mode)
+    {
+      case LEFT_ANALOG_STICK:
+        leftstick_control(cmd);
+        break;
+      case LEFT_RIGHT_STICK:
+        leftrightstick_control(cmd);
+        break;
+      case LEFT_PAD_KEY:
+        leftkey_control(cmd);
+        break;
+      case RIGHT_PAD_KEY:
+        rightkey_control(cmd);
+        break;
+    }
+  }
+}
+
+void auto_control()
+{
+  for (int index = 0; index < EEPROM.length() ; index++) {
+    if (EEPROM[index] == 0) {
+      btcar->stop();
+      break;
+    }
+    learn_control(EEPROM[index]);
+    delay(EEPROM[++index]);
+  }
+}
+
+void learning(char cmd)
+{
+  switch (cmd)
+  {
+    case 'I':
+    case 'J':
+    case 'K':
+    case 'L':
+    case 'H':
+      if (rom_addr < EEPROM.length())
+      {
+        if (begin_time != 0)
+          EEPROM.write(rom_addr++, millis() - begin_time);
+        EEPROM.write(rom_addr++, cmd);
+        learn_control(cmd);
+        begin_time = millis();
+      }
+      break;
   }
 }
 
 void leftstick_control(char cmd)
 {
-  switch(cmd)
+  switch (cmd)
   {
     case 'W':
       long speed;
       speed = Serial.parseInt();
-      
+
       /*
-      127 is the center position value, 
-      top position is min value = 0, 
-      bottom position is max value = 255,
-      easy way: <127 go forward, =127 stop, >127 go backward
-      analog speed change: 
-      0: go forward with full speed
-      127: speed is 0
-      255: go backward with full speed
-      go forward speed formula: (127-speed)/(127-0)*255
-      go backward speed formula: speed/(255-127)*255
+        127 is the center position value,
+        top position is min value = 0,
+        bottom position is max value = 255,
+        easy way: <127 go forward, =127 stop, >127 go backward
+        analog speed change:
+        0: go forward with full speed
+        127: speed is 0
+        255: go backward with full speed
+        go forward speed formula: (127-speed)/(127-0)*255
+        go backward speed formula: speed/(255-127)*255
       */
       if (speed < 127)
       {
-        //btcar->changeSpeed(255 - 2 * speed); 
+        //btcar->changeSpeed(255 - 2 * speed);
         btcar->run(FOR);
       }
       else if (speed > 127)
@@ -123,7 +208,7 @@ void leftstick_control(char cmd)
         //btcar->changeSpeed(2 * speed);
         btcar->run(BACK);
       }
-      else if (speed ==127)
+      else if (speed == 127)
       {
         btcar->stop();
       }
@@ -133,13 +218,13 @@ void leftstick_control(char cmd)
       angle = Serial.parseInt();
 
       /*
-      128 is the center position value, 
-      left position is min value = 0, 
-      right position is max value = 255,
-      analog speed change: 
-      0: map to servo 0
-      128: map to servo 90
-      255: map to servo 180
+        128 is the center position value,
+        left position is min value = 0,
+        right position is max value = 255,
+        analog speed change:
+        0: map to servo 0
+        128: map to servo 90
+        255: map to servo 180
       */
       car4wd.changeAngle(angle * 180 / 256);
       break;
@@ -148,23 +233,23 @@ void leftstick_control(char cmd)
 
 void leftrightstick_control(char cmd)
 {
-  switch(cmd)
+  switch (cmd)
   {
     case 'W':
       long speed;
       speed = Serial.parseInt();
-      
+
       if (speed < 127)
       {
-        //btcar->changeSpeed(255 - 2 * speed); 
+        btcar->changeSpeed(255 - 2 * speed);
         btcar->run(FOR);
       }
       else if (speed > 127)
       {
-        //btcar->changeSpeed(2 * speed);
+        btcar->changeSpeed(2 * speed);
         btcar->run(BACK);
       }
-      else if (speed ==127)
+      else if (speed == 127)
       {
         btcar->stop();
       }
@@ -179,53 +264,86 @@ void leftrightstick_control(char cmd)
 
 void leftkey_control(char cmd)
 {
-  switch(cmd)
-    {
-      case 'f':
-      case 'A':
-        btcar->run(FOR);
-        //Serial.println("Run Forward");
-        break;
-      case 'b':
-      case 'B':
-        btcar->run(BACK);
-        //Serial.println("Run Backward");
-        break;
-      case 'l':
-      case 'C':
-        btcar->turn(BACK);
-        //Serial.println("Turn Left");
-        break;
-      case 'r':
-      case 'D':
-        btcar->turn(FOR);
-        //Serial.println("Turn Right");
-        break;
-    }
+  switch (cmd)
+  {
+    case 'f':
+    case 'A':
+      btcar->run(FOR);
+      //Serial.println("Run Forward");
+      break;
+    case 'b':
+    case 'B':
+      btcar->run(BACK);
+      //Serial.println("Run Backward");
+      break;
+    case 'l':
+    case 'C':
+      btcar->turn(BACK);
+      //Serial.println("Turn Left");
+      break;
+    case 'r':
+    case 'D':
+      btcar->turn(FOR);
+      //Serial.println("Turn Right");
+      break;
+  }
 }
 
 void rightkey_control(char cmd)
 {
-  switch(cmd)
-    {
-      case 'I':
-        btcar->run(FOR);
-        //Serial.println("Run Forward");
-        break;
-      case 'J':
-        btcar->run(BACK);
-        //Serial.println("Run Backward");
-        break;
-      case 'K':
-        car4wd.changeAngle(car4wd.currentAngle() - 90);
-        //Serial.println("Turn Left");
-        break;
-      case 'L':
-        car4wd.changeAngle(car4wd.currentAngle() + 90);
-        //Serial.println("Turn Right");
-        break;
-      case 'E':
-        btcar->stop();
-        break;
-    }
+  switch (cmd)
+  {
+    case 'I':
+      btcar->run(FOR);
+      //Serial.println("Run Forward");
+      break;
+    case 'J':
+      btcar->run(BACK);
+      //Serial.println("Run Backward");
+      break;
+    case 'K':
+      car4wd.changeAngle(car4wd.currentAngle() - 90);
+      //Serial.println("Turn Left");
+      break;
+    case 'L':
+      car4wd.changeAngle(car4wd.currentAngle() + 90);
+      //Serial.println("Turn Right");
+      break;
+    case 'E':
+      btcar->stop();
+      break;
+  }
+}
+
+void learn_control(char cmd)
+{
+  switch (cmd)
+  {
+    case 'I':
+      btcar->run(FOR);
+      //Serial.println("Run Forward");
+      break;
+    case 'J':
+      btcar->run(BACK);
+      //Serial.println("Run Backward");
+      break;
+    case 'K':
+      car4wd.changeAngle(car4wd.currentAngle() - 90);
+      //Serial.println("Turn Left");
+      break;
+    case 'L':
+      car4wd.changeAngle(car4wd.currentAngle() + 90);
+      //Serial.println("Turn Right");
+      break;
+    case 'H':
+      btcar->stop();
+      break;
+  }
+}
+
+void clear_eeprom()
+{
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
 }
--- arduino/messagedebug/messagedebug.ino
+++ arduino/messagedebug/messagedebug.ino
@@ -1,10 +1,31 @@
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 
 #define GAMEPAD_BAUD_RATE 9600
 #define BLUETOOTH_BAUD_RATE 115200
+#define INDICATOR_PIN 2
+
+enum PadMode
+{
+  LEFT_ANALOG_STICK = 0,
+  LEFT_RIGHT_STICK = 1,
+  LEFT_PAD_KEY = 2,
+  RIGHT_PAD_KEY = 3
+};
+
+enum CarMode
+{
+  MANUAL = 0,
+  LEARN = 1,
+  AUTO = 2
+};
 
 SoftwareSerial softSerial(10, 11); // RX, TX
-bool autoMode = false;
+PadMode manual_mode = LEFT_ANALOG_STICK;
+CarMode control_mode = MANUAL;
+int rom_addr = 0;
+unsigned long begin_time;
+int currentAngle = 90;
 
 void setup() {
   Serial.begin(BLUETOOTH_BAUD_RATE);
@@ -13,6 +34,9 @@ void setup() {
   }
   softSerial.begin(GAMEPAD_BAUD_RATE);
   Serial.println("Hello, monitor");
+  pinMode(INDICATOR_PIN, OUTPUT);
+  digitalWrite(INDICATOR_PIN, LOW);
+  softSerial.setTimeout(300);
 }
 
 void loop() {
@@ -21,75 +45,258 @@ void loop() {
 
 void processMessage()
 {
-  char cmd[1];
-  if (softSerial.readBytes(cmd, 1))
+  char msg[1];
+  if (softSerial.readBytes(msg, 1))
   {
+    char cmd = msg[0];
     Serial.println(cmd);
-    switch(cmd[0])
+    if (cmd == 'G')
     {
-      case 'f':
-      case 'A':
-        Serial.println("Run Forward");
-        break;
-      case 'b':
-      case 'B':
-        Serial.println("Run Backward");
-        break;
-      case 'l':
-      case 'C':
-        Serial.println("Turn Left");
-        break;
-      case 'r':
-      case 'D':
-        Serial.println("Turn Right");
-        break;
-      case 's':
-      case 'E':
-        Serial.println("STOPPED");
-        break;
-      case 'F':
-      case 'G':
-      case 'H':
-      case 'I':
-      case 'J':
-        Serial.println("Unkown command");
-        break;
-      case 'K':
-        Serial.println("U-Turn Left");
-        break;
-      case 'L':
-        Serial.println("U-Turn Right");
-        break;
-      case 'M':
-      case 'N':
-        Serial.println("Reserved command for auto mode switch");
-        autoMode = !autoMode;
-        break;
-      case 'W':
-        long speed;
-        speed = softSerial.parseInt();
-        
-        if (speed > 127)
+      control_mode = CarMode((control_mode + 1) % 3);
+      switch (control_mode)
+      {
+        case MANUAL:
+          digitalWrite(INDICATOR_PIN, LOW);
+          Serial.println("switch to manual mode");
+          break;
+        case LEARN:
+          clear_eeprom();
+          rom_addr = 0;
+          begin_time = 0;
+          digitalWrite(INDICATOR_PIN, HIGH);
+          Serial.println("switch to learning mode");
+          break;
+        case AUTO:
+          digitalWrite(INDICATOR_PIN, HIGH);
+          Serial.println("switch to auto mode");
+          break;
+      }
+    }
+    else
+    {
+      switch (control_mode)
+      {
+        case MANUAL:
+          manualControl(cmd);
+          break;
+        case AUTO:
+          if (cmd == 'H')
+          {
+            Serial.println("start auto run");
+            autoControl();
+          }
+          break;
+        case LEARN:
+          Serial.println("start learning the route...");
+          learning(cmd);
+          break;
+      }
+    }
+  }
+  else if (control_mode == MANUAL && manual_mode == LEFT_PAD_KEY)
+  {
+    Serial.println("Stop car for serial timeout");
+  }
+  else if (control_mode == LEARN)
+  {
+    //Serial.println("Blink the led to indicate this is learn mode now");
+    digitalWrite(INDICATOR_PIN, LOW);
+    delay(500);
+    digitalWrite(INDICATOR_PIN, HIGH);
+  }
+}
+
+void manualControl(char cmd)
+{
+  if (cmd == 'H')
+  {
+    manual_mode = PadMode((manual_mode + 1) % 4);
+  }
+  else
+  {
+    switch (manual_mode)
+    {
+      case LEFT_ANALOG_STICK:
+        Serial.println("use left analog stick to control the car");
+        switch (cmd)
         {
-          Serial.print("go forward with speed: ");
-          Serial.println(speed);
+          case 'W':
+            long speed;
+            speed = Serial.parseInt();
+            if (speed < 127)
+              Serial.println("run forward with full speed");
+            else if (speed > 127)
+              Serial.println("run backward with full speed");
+            else if (speed == 127)
+              Serial.println("car stopped");
+            break;
+          case 'P':
+            long angle;
+            angle = Serial.parseInt();
+            Serial.print("wheel the car to angle ");
+            Serial.println(angle);
+            break;
         }
-        else if (speed < 127)
-        {
-          Serial.print("go backward with speed: ");
-          Serial.println(speed);
-        }
-        else if (speed ==127)
-          Serial.print("got speed value: ");
-          Serial.print(speed);
-          Serial.println(";, stoped");
         break;
-      case 'P':
-        long angle;
-        angle = softSerial.parseInt();
-        Serial.print("Turn to angle: ");
-        Serial.println(angle);
+      case LEFT_RIGHT_STICK:
+        Serial.println("use left analog stick to control the car speed");
+        Serial.println("use right analog stick to control the car angle");
+        switch (cmd)
+        {
+          case 'W':
+            long speed;
+            speed = Serial.parseInt();
+
+            if (speed < 127)
+            {
+              Serial.print("go forward with speed ");
+              Serial.println(255 - 2 * speed);
+            }
+            else if (speed > 127)
+            {
+              Serial.print("go backward with speed ");
+              Serial.println(2 * speed);
+            }
+            else if (speed == 127)
+            {
+              Serial.println("car stopped");
+            }
+            break;
+          case 'S':
+            long angle;
+            angle = Serial.parseInt();
+            Serial.print("wheel the car to angle ");
+            Serial.println(angle);
+            break;
+        }
+        break;
+      case LEFT_PAD_KEY:
+        Serial.println("use left pad key to control the car");
+        switch (cmd)
+        {
+          case 'f':
+          case 'A':
+            Serial.println("Run Forward");
+            break;
+          case 'b':
+          case 'B':
+            Serial.println("Run Backward");
+            break;
+          case 'l':
+          case 'C':
+            Serial.println("Turn Left");
+            break;
+          case 'r':
+          case 'D':
+            Serial.println("Turn Right");
+            break;
+        }
+        break;
+      case RIGHT_PAD_KEY:
+        Serial.println("use right pad key to control the car");
+        switch (cmd)
+        {
+          case 'I':
+            Serial.println("Run Forward");
+            break;
+          case 'J':
+            Serial.println("Run Backward");
+            break;
+          case 'K':
+            Serial.print("turn to angle ");
+            Serial.println(currentAngle - 90);
+            break;
+          case 'L':
+            Serial.print("turn to angle ");
+            Serial.println(currentAngle + 90);
+            break;
+          case 'E':
+            Serial.println("car stopped");
+            break;
+        }
         break;
     }
+  }
+}
+
+void autoControl()
+{
+  for (int index = 0; index < EEPROM.length() ; index++) {
+    if (EEPROM[index] == 0) {
+      Serial.print("EEPROM[index] = ");
+      Serial.println(EEPROM[index]);
+      break;
+    }
+    Serial.println(char(EEPROM[index]));
+    learn_control(EEPROM[index]);
+    byte high = EEPROM.read(++index);
+    byte low = EEPROM.read(++index);
+    int duration = word(high, low);
+    Serial.println(duration);
+    delay(duration);
+  }
+}
+
+void learning(char cmd)
+{
+  switch (cmd)
+  {
+    case 'I':
+    case 'J':
+    case 'K':
+    case 'L':
+    case 'H':
+      if (rom_addr < EEPROM.length())
+      {
+        if (begin_time != 0) {
+          int duration = millis() - begin_time;
+          EEPROM.write(rom_addr++, highByte(duration));
+          EEPROM.write(rom_addr++, lowByte(duration));
+          Serial.println(millis() - begin_time);
+        }
+        //        Serial.print("EEPROM[");
+        //        Serial.print(rom_addr-1);
+        //        Serial.print("]=");
+        //        Serial.println(EEPROM[rom_addr-1]);
+        EEPROM.write(rom_addr++, cmd);
+        //        Serial.print("EEPROM[");
+        //        Serial.print(rom_addr-1);
+        //        Serial.print("]=");
+        //        Serial.println(EEPROM[rom_addr-1]);
+        learn_control(cmd);
+        begin_time = millis();
+      }
+      break;
+  }
+}
+
+void learn_control(char cmd)
+{
+  switch (cmd)
+  {
+    case 'I':
+      Serial.println("Run Forward with full speed in auto mode");
+      break;
+    case 'J':
+      Serial.println("Run Backward with full speed in auto mode");
+      break;
+    case 'K':
+      Serial.print("[AUTO MODE]turn to angle ");
+      Serial.println(currentAngle - 90);
+      break;
+    case 'L':
+      Serial.print("[AUTO MODE]turn to angle ");
+      Serial.println(currentAngle + 90);
+      break;
+    case 'H':
+      Serial.print("[AUTO MODE]Stop the car");
+      break;
+  }
+}
+
+void clear_eeprom()
+{
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
   }
 }
