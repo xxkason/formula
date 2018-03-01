0dec03ed33535397ad4789f0e2678bfe440c0140
Kason Kai Peng <kpeng@rockwellautomation.com>
2018-03-01 16:33:33 +0800

    add interrupts for sensor

--- arduino/formula/formula.ino
+++ arduino/formula/formula.ino
@@ -46,6 +46,40 @@ CarMode carMode = MANUAL;
 int romAddr = 0;
 unsigned long beginTime;
 
+// Below is a list of interrupts, in priority order, for the Atmega328:
+
+//  1  Reset 
+//  2  External Interrupt Request 0  (pin D2)          (INT0_vect)
+//  3  External Interrupt Request 1  (pin D3)          (INT1_vect)
+//  4  Pin Change Interrupt Request 0 (pins D8 to D13) (PCINT0_vect)
+//  5  Pin Change Interrupt Request 1 (pins A0 to A5)  (PCINT1_vect)
+//  6  Pin Change Interrupt Request 2 (pins D0 to D7)  (PCINT2_vect)
+//  7  Watchdog Time-out Interrupt                     (WDT_vect)
+//  8  Timer/Counter2 Compare Match A                  (TIMER2_COMPA_vect)
+//  9  Timer/Counter2 Compare Match B                  (TIMER2_COMPB_vect)
+// 10  Timer/Counter2 Overflow                         (TIMER2_OVF_vect)
+// 11  Timer/Counter1 Capture Event                    (TIMER1_CAPT_vect)
+// 12  Timer/Counter1 Compare Match A                  (TIMER1_COMPA_vect)
+// 13  Timer/Counter1 Compare Match B                  (TIMER1_COMPB_vect)
+// 14  Timer/Counter1 Overflow                         (TIMER1_OVF_vect)
+// 15  Timer/Counter0 Compare Match A                  (TIMER0_COMPA_vect)
+// 16  Timer/Counter0 Compare Match B                  (TIMER0_COMPB_vect)
+// 17  Timer/Counter0 Overflow                         (TIMER0_OVF_vect)
+// 18  SPI Serial Transfer Complete                    (SPI_STC_vect)
+// 19  USART Rx Complete                               (USART_RX_vect)
+// 20  USART, Data Register Empty                      (USART_UDRE_vect)
+// 21  USART, Tx Complete                              (USART_TX_vect)
+// 22  ADC Conversion Complete                         (ADC_vect)
+// 23  EEPROM Ready                                    (EE_READY_vect)
+// 24  Analog Comparator                               (ANALOG_COMP_vect)
+// 25  2-wire Serial Interface  (I2C)                  (TWI_vect)
+// 26  Store Program Memory Ready                      (SPM_READY_vect)
+
+ISR (PCINT1_vect)
+{
+  if (PINA)
+}
+
 void setup()
 {
   Serial.begin(GAMEPAD_BAUD_RATE);
@@ -60,6 +94,8 @@ void setup()
   digitalWrite(CAR_MODE_INDICATOR_PIN, LOW);
   pinMode(PAD_MODE_INDICATOR_PIN, OUTPUT);
   digitalWrite(PAD_MODE_INDICATOR_PIN, HIGH);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
   Serial.setTimeout(300);
 }
 
